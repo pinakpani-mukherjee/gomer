1f3d54d38f274e7baa333244d48e371837a3e97f
mukherjee-pinakpani <mukherjee.pinakpani14@gmail.com>
2024-02-25 15:32:53 +0900

    creating possible changes for data format

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"go.etcd.io/bbolt"
+)
 
 func main() {
+	db, err := bbolt.Open(".db", 0666, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	user := map[string]string{
+		"name":"Pinak",
+		"age": "28",
+	} 
+
+	db.Update(func(tx *bbolt.Tx) error {
+		bucket, err := tx.CreateBucket([]byte("users"))
+		if err != nil {
+			return err
+		}
+
+		for k, v := range user {
+			if err := bucket.Put([]byte(k),[]byte(v)); err !=nil {
+				return err
+			}
+		}
+
+		bucket.
+		return nil
+	})
+
 	fmt.Println("Hello world!!!")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/pinakpani-mukherjee/gomer
 
 go 1.22.0
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	go.etcd.io/bbolt v1.3.9 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+)
